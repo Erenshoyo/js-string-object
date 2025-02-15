26dd6627ced1e809af8820b799c157e1aee1465f
Md. Asif Shahriar Tauhid <70537695+Erenshoyo@users.noreply.github.com>
2025-02-15 20:59:41 +0600

    js-string-object

---
+++ object.js
@@ -0,0 +1,27 @@
+//Primitive --> Basic level data type
+
+const age = 21;
+const school = 'Safiuddin Sarker'
+const isPassed = true;
+// let isDeveloper;
+const subjects = ['bangla', 'english', 'physics', 'math'];
+
+// object
+// Non-primitive
+const bottle = {
+    brand: 'apple',
+    price: 45,
+    color: 'white',
+    isClean: false
+}
+
+const subject = {
+    name: 'Biology',
+    teacher: 'Ansari Sir',
+    examDate: '27feb',
+    chapters: ['first', 'second', 'third'],
+    exams: {
+        name: 'Final Exam',
+        marks: 100
+    }
+}
